31b06132fd657b5e796ecd90bd46e7c751624255
Gabor Szabo <gabor@szabgab.com>
2021-05-04 11:00:07 +0300

    implement front-end of adding a new workout

--- src/components/WorkoutCreator.jsx
+++ src/components/WorkoutCreator.jsx
@@ -2,6 +2,36 @@ import React, { useEffect } from 'react';
 
 export default function WorkoutCreator() {
   const [allExercises, setAllExercises] = React.useState([]);
+  const [selectedExercises, setSelectedExercises] = React.useState({});
+  const [workoutName, setWorkoutName] = React.useState('');
+  const handleChange = (e) => {
+    // console.log(e);
+    // console.log(e.target.checked);
+    // console.log(e.target.value);
+    setSelectedExercises({ ...selectedExercises, [e.target.value]: e.target.checked });
+    // console.log(selectedExercises);
+  };
+  const handleNameChange = (e) => {
+    // console.log(e.target.value);
+    setWorkoutName(e.target.value);
+    // console.log(workoutName);
+  };
+  const handleSubmit = (event) => {
+    // console.log(workoutName);
+    // console.log(selectedExercises);
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+          title: workoutName,
+          exercises: selectedExercises
+       })
+    };
+    fetch('/api/workout', requestOptions)
+      .then(response => response.json())
+      .then(data => this.setState({ postId: data.id }));
+    event.preventDefault();
+  };
 
   useEffect(() => {
     fetch('/api/exercises').then(res => res.json()).then(data => {
@@ -10,16 +40,20 @@ export default function WorkoutCreator() {
   }, []);
 
   return allExercises.length > 0 &&
-    (<form>
+    (<form onSubmit={handleSubmit}>
+      <label>Workout name: <input type="textinput" name="workoutname" onChange={handleNameChange} /></label>
+      <ul>
       {
         allExercises.map((exercise) => {
           return (
-            <label key={exercise.id}>
+            <li key={exercise.id}><label>
+              <input type="checkbox" name={exercise.name} value={exercise.id} onChange={handleChange} />
               {exercise.name}
-              <input type="checkbox" name={exercise.name} value={exercise.id}/>
-            </label>
+            </label></li>
           );
         })
       }
+      </ul>
+      <input type="submit" value="Save" />
     </form>);
 }
